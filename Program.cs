c081e5be2c809774386472bb9fb252ff4fb9ce43
Fabian Quesada <72717982+faqu@users.noreply.github.com>
2025-06-18 11:03:54 -0600

    Update to code add a read to ge the number digited by user and added the bases to deveolp the method OldPhone Pad

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace OldPhoneApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Digit The numbers");
+            string Numb = Console.ReadLine();
+            Console.WriteLine(OldPhonePad(Numb));
+        }
+        public static String OldPhonePad(string input)
+        {
+            var Dict = new Dictionary<string, List<string>>()
+            {
+                { "1", new List<string> { "&", "'", "(" } },
+                { "2", new List<string> { "A", "B", "C" } },
+                { "3", new List<string> { "D", "E", "F" } },
+                { "4", new List<string> { "G", "H", "I" } },
+                { "5", new List<string> { "J", "K", "L" } },
+                { "6", new List<string> { "M", "N", "O" } },
+                { "7", new List<string> { "P", "Q", "R","S" } },
+                { "8", new List<string> { "T", "U", "V" } },
+                { "9", new List<string> { "W", "X", "Y","Z" } },
+                { "*", new List<string> { "DEL"} },
+                { "0", new List<string> { "ESP"} },
+                { ">", new List<string> { "#"} },
+            };
+
+            string Tester = "Tester"; 
+            return Tester;
+            
+        }
+    }
+}
